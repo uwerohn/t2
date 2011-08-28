ce29881cfd7f81df4cb42ca35acea45d764c22cd
Besitzer <uwe.rohn@bigfoot.com>
2011-08-28 08:02:27 +0200

    TI2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+T2::Application.config.secret_token = 'c72162c13d86825cc589f534c05249e80f6d85e7f5c3bf0cc1ce0881bd28d39a70a6c93bdde5cb5c5a452480ef2fef0cbe2bea86ede8096d674ec872fa34a3a7'
